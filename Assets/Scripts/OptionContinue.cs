547b8acf41ca0f3613380ebe4cc162d379fd07be
hushion-r <shushion@macalester.edu>
2020-04-15 14:59:24 -0500

    First commit. Scenario change functionality implemented

---
+++ Assets/Scripts/OptionContinue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionContinue : MonoBehaviour {
+    public Button button;
+    public GameObject thisScenario;
+    public GameObject nextScenario;
+
+    void Awake() {
+        button.onClick.AddListener(SwitchScenario);
+    }
+
+    void SwitchScenario() {
+        thisScenario.SetActive(false);
+        nextScenario.SetActive(true);
+    }
+}
