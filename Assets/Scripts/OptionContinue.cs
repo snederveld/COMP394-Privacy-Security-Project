31fc1d24403237ed6f06482c92a3cc9c6512402b
snederveld <snederve@macalester.edu>
2020-04-19 10:29:33 -0500

    Updated the prefab and made LockImagery scenario

--- Assets/Scripts/OptionContinue.cs
+++ Assets/Scripts/OptionContinue.cs
@@ -6,20 +6,37 @@ using UnityEngine.UI;
 public class OptionContinue : MonoBehaviour {
     public Button button;
     public bool correct;
-    public GameObject thisScenario;
-    public GameObject nextScenario;
+    public Text failText;
+    public Text correctText;
+    //public GameObject thisScenario;
+    //public GameObject nextScenario;
 
     void Awake() {
-        button.onClick.AddListener(SwitchScenario);
+        //button.onClick.AddListener(SwitchScenario);
+        button.onClick.AddListener(ChangeText);
+        failText.enabled = false;
+        correctText.enabled = false;
     }
 
-    void SwitchScenario() {
-        if (correct) {    // switch scenario
-            thisScenario.SetActive(false);
-            nextScenario.SetActive(true);
-        } else {    // show Failure GO
-            GameObject failureGO = GameObject.Find("Failure");
-            failureGO.transform.GetChild(0).gameObject.SetActive(true);
+    void ChangeText()
+    {
+        if (correct)
+        {
+            correctText.enabled = true;
+        }
+        else
+        {
+            failText.enabled = true;
         }
     }
+
+    //void SwitchScenario() {
+    //    if (correct) {    // switch scenario
+    //        thisScenario.SetActive(false);
+    //        nextScenario.SetActive(true);
+    //    } else {    // show Failure GO
+    //        GameObject failureGO = GameObject.Find("Failure");
+    //        failureGO.transform.GetChild(0).gameObject.SetActive(true);
+    //    }
+    //}
 }
