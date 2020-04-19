11511a75d77151d291c8a13169bc38a68aff67a5
snederveld <snederve@macalester.edu>
2020-04-19 09:56:06 -0500

    All my stuff (hopefully this works)

---
+++ Assets/Scripts/TextScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextScript : MonoBehaviour {
+    public Button button;
+    public GameObject thisScenario;
+    public GameObject nextScenario;
+
+    void Awake() {
+        button.onClick.AddListener(ChangeScenario);
+    }
+
+    void ChangeScenario() {
+        if (thisScenario.name != nextScenario.name) {
+            thisScenario.SetActive(false);
+            nextScenario.SetActive(true);
+        }
+        else {
+            Scene loadedLevel = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(loadedLevel.buildIndex);
+        }
+
+    }
+}
